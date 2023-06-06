6fa84f766125f19908b8f0daa39434b161942a8e
yashweblife <yashweblife20@gmail.com>
2023-06-05 23:45:41 -0500

    add subtract

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
+use std::ops::{Add, Sub};
 struct Vector{
-    x:u32,
-    y:u32
+    x:f64,
+    y:f64
 }
 
 impl Vector {
-    fn mag(&self)->u32{
-        self.x*self.x + self.y*self.y
+    fn new(x:f64, y:f64)->Vector{
+        Vector{x,y}
     }
+    fn mag(&self)->f64{
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+impl Add<Vector> for Vector{
+    type Output = Vector;
+    fn add(self, other: Vector)->Vector{
+        Vector::new(self.x+other.x, self.y+other.y)
+    }
+}
+impl Sub<Vector> for Vector{
+    type Output = Vector;
+    fn sub(self, other: Vector)->Vector{
+        Vector::new(self.x-other.x, self.y-other.y)
+    }
+}
+
+fn main(){
+    println!("HELLO WORLD");
 }
