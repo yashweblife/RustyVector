a1b006a824e26d31323e33e7fe5a27d9eb24eafa
yashweblife <yashweblife20@gmail.com>
2023-06-06 22:04:11 -0500

    add more functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::ops::{Add, Sub};
+use std::ops::{Add, Sub, Mul, Div};
+use std::fmt;
 struct Vector{
     x:f64,
     y:f64
@@ -8,8 +9,11 @@ impl Vector {
     fn new(x:f64, y:f64)->Vector{
         Vector{x,y}
     }
-    fn mag(&self)->f64{
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+}
+
+impl fmt::Display for Vector{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        write!(f, "<{}, {}>",self.x, self.y)
     }
 }
 impl Add<Vector> for Vector{
@@ -24,7 +28,24 @@ impl Sub<Vector> for Vector{
         Vector::new(self.x-other.x, self.y-other.y)
     }
 }
-
+impl Mul<Vector> for Vector{
+    type Output = Vector;
+    fn mul(self, other: Vector)->Vector{
+        Vector::new(self.x*other.x, self.y*other.y)
+    }
+}
+impl Div<Vector> for Vector{
+    type Output = Vector;
+    fn div(self, other:Vector)->Vector{
+        Vector::new(self.x/other.x, self.y/other.y)
+    }
+}
+fn mag(v:Vector)->f64{
+    (v.x*v.x + v.y*v.y).sqrt()
+}
 fn main(){
-    println!("HELLO WORLD");
+    let v1 = Vector::new(10.0,10.0);
+    let v2 = Vector::new(10.0,10.0);
+    let sum = v1 + v2;
+    println!("{}", mag(v1));
 }
